a05d4a2f15b3590c5c76b283381840c01290e5b0
Rashedul Alam Anik <rashedulalam112112@gmail.com>
2022-11-25 10:45:57 +0600

    Setup keycloak with Vue

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,50 @@
 import { createApp } from "vue";
+import Keycloak from "keycloak-js";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
-const app = createApp(App);
+let initOptions = {
+  url: "http://localhost:8080/auth",
+  realm: "keycloak-demo",
+  clientId: "app-vue",
+  onLoad: "login-required",
+};
 
-app.use(router);
+let keycloak = new Keycloak(initOptions);
 
-app.mount("#app");
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.log(":D");
+  }
+
+  const app = createApp(App);
+  app.use(router);
+  app.mount("#app");
+
+  setInterval(() => {
+    keycloak
+      .updateToken(70)
+      .then((refreshed) => {
+        if (refreshed) {
+          console.log("Token refreshed" + refreshed);
+        } else {
+          console.warn(
+            "Token not refreshed, valid for " +
+              Math.round(
+                keycloak.tokenParsed.exp +
+                  keycloak.timeSkew -
+                  new Date().getTime() / 1000
+              ) +
+              " seconds"
+          );
+        }
+      })
+      .error(() => {
+        console.error("Failed to refresh token");
+      });
+  }, 60000);
+});
